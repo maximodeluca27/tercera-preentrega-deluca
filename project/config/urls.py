c5816eebed181558313b97cd4f155df8fcff2eb3
maximodeluca27 <maximodeluca644@gmail.com>
2024-05-12 00:47:11 -0300

    agrego clase avion

--- project/config/urls.py
+++ project/config/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludo/',views.saludo),
+    path('saludo/',views.probandotemplate),
 ]
--- project/config/views.py
+++ project/config/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def saludo(request):
     return HttpResponse("Hola Maxi")
+
+def probandotemplate(request):
+  datos={"saludo":"Bienvenido a VirtualPlanes","autor":"Maximo De Luca"}
+  return render(request,"template.html",context=datos)
