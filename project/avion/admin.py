43cee4ca02f540cd2a83091a58d57a5ab62e1ede
maximodeluca27 <maximodeluca644@gmail.com>
2024-05-12 23:16:14 -0300

    agrego aeropuerto

--- project/avion/admin.py
+++ project/avion/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Cliente,Avion
+from .models import Cliente,Avion,Aeropuerto
 
 admin.site.register(Cliente)
+admin.site.register(Aeropuerto)
 admin.site.register(Avion)
+
 # Register your models here.
--- project/avion/models.py
+++ project/avion/models.py
@@ -6,11 +6,19 @@ class Cliente(models.Model):
 
     def __str__(self) -> str:
         return self.nombre
+    
+class Aeropuerto(models.Model):
+    nombre=models.CharField(max_length=40)
+    def __str__(self) -> str:
+        return self.nombre
 
 class Avion(models.Model):
     fabricante=models.CharField(max_length=40)
     modelo=models.CharField(max_length=40)
+    avion_origen_id=models.ForeignKey(Aeropuerto,on_delete=models.SET_NULL,null=True)
 
     def __str__(self) -> str:
         return f"{self.fabricante} {self.modelo} "
+    
+
 # Create your models here.
