ff94fe9714e4a6b74aa5fff498934a99fbd827d2
maximodeluca27 <maximodeluca644@gmail.com>
2024-05-12 22:57:25 -0300

    agrego clases registradas y admin

--- project/avion/admin.py
+++ project/avion/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cliente,Avion
 
+admin.site.register(Cliente)
+admin.site.register(Avion)
 # Register your models here.
--- project/avion/models.py
+++ project/avion/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Cliente(models.Model):
+    nombre=models.CharField(max_length=40)
+    pasajes=models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Avion(models.Model):
+    fabricante=models.CharField(max_length=40)
+    modelo=models.CharField(max_length=40)
+
+    def __str__(self) -> str:
+        return f"{self.fabricante} {self.modelo} "
 # Create your models here.
