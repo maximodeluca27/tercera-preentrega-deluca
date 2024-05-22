772a5f4c933451ee6dce101f19f1bdde2b61ff2a
maximodeluca27 <maximodeluca644@gmail.com>
2024-05-22 16:54:42 -0300

    agrego migraciones nuevas

--- project/avion/forms.py
+++ project/avion/forms.py
@@ -5,7 +5,7 @@ from . import models
 class AvionForm(forms.ModelForm):
     class Meta: 
         model=models.Avion
-        fields={"fabricante","modelo","avion_origen_id"}
+        fields={"fabricante","modelo","avion_origen_id","fecha_incorporacion","capacidad_maxima"}
 
 class AeropuertoForm(forms.ModelForm):
     class Meta: 
--- project/avion/models.py
+++ project/avion/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Cliente(models.Model):
     nombre=models.CharField(max_length=40)
@@ -17,6 +18,8 @@ class Avion(models.Model):
     fabricante=models.CharField(max_length=40)
     modelo=models.CharField(max_length=40)
     avion_origen_id=models.ForeignKey(Aeropuerto,on_delete=models.SET_NULL,null=True)
+    fecha_incorporacion=models.DateField(default=timezone.now)
+    capacidad_maxima=models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return f"{self.fabricante} {self.modelo} "
