698608cfedfca9bf7c32f15db3fef5b065b8efd4
maximodeluca27 <maximodeluca644@gmail.com>
2024-05-23 15:29:03 -0300

    agrego registrarse y contacto

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import index, CustomLoginView
+from core.views import index, CustomLoginView, contacto, register
 from django.contrib.auth.views import LogoutView
 
 app_name="core"
@@ -8,4 +8,6 @@ urlpatterns=[
     path("",index,name="index"),
     path("login/",CustomLoginView.as_view(),name="login"),
     path("logout/",LogoutView.as_view(template_name="core/logout.html"),name="logout"),
+    path("contacto/",contacto,name="contacto" ),
+    path("register/",register,name="register")
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
-from .forms import CustomAuthenticationForm
+from .forms import CustomAuthenticationForm,CustomUserCreationForm
+from django.http import HttpRequest,HttpResponse
+
 
 def index(request):
     return render(request,"core/index.html")
 
+def contacto(request):
+    return render(request,"core/contacto.html")
+
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
     template_name="core/login.html"
+
+def register(request:HttpRequest)->HttpResponse:
+    if request.method=='POST':
+        form=CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data["username"]
+            form.save()
+            return render(request,"core/index.html",{"mensaje":f"Usuario '{username}' creado "})
+    else:
+        form=CustomUserCreationForm()
+    return render(request,"core/register.html",{"form":form})
